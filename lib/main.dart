604c0f1a080aa87a47347a9cff3c4f3006b42f2f
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2019-08-30 10:43:45 -0300

    Primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+	runApp(
+		MaterialApp(
+			title: "Fases do dia",
+			home: Container(color: Colors.white),
+		)
+	);
+}
