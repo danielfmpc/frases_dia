7ff039d24f1fadcb6dedcbda4e11fa456bee0553
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2019-08-30 15:41:52 -0300

    Colocando o botão

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,24 @@ void main() {
 				color: Colors.white,
 				child: Column(
 					children: <Widget>[
-						Text(
-							"Osso",
-							style: TextStyle(
-								fontSize: 35,
-								color: Colors.black,
-								fontStyle: FontStyle.normal,
-								fontWeight: FontWeight.bold,
-							),
+						FlatButton(
+							onPressed: (){
+								print("Pressionado");
+							},
+							child:Text(
+								"Clique aqui",
+								style: TextStyle(
+									fontSize: 35,
+									color: Colors.black,
+									fontStyle: FontStyle.normal,
+									fontWeight: FontWeight.bold,
+									wordSpacing: 0, // Serve para ajustar o espaço entre as palavras
+									decoration: TextDecoration.none, // serve para tirar o sublinhado
+									decorationColor: Colors.black, // Serve para dar cor ao sublinhado
+									decorationStyle: TextDecorationStyle.solid, // Serve para definir a quantidade de linha do sublinhado
+									letterSpacing: 0 // A quantidade de espaço a ser adicionado entre cada letra. Um valor negativo pode ser usado para aproximar as letras.
+								),
+							),	
 						),
 					],
 				),
